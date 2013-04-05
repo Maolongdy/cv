a0d2238e2350a40bbb684fc3b2dc7a05429bba70
Mao Pechenda <maolongdy@ubuntu.ubuntu-domain>
2013-04-05 09:17:04 +0700

    inititailize commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CV::Application.config.secret_token = '463fd0ed3ddbde748c35461f9a3f774fdcd5b5500a09ed565ad79729338a826f424ee13404d4f0c65dd9aaf7a9c42d764ab6625acb6945fe7ff35b2fe11e5816'
